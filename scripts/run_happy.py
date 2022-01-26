ac6eab8d352fd2b2ab5c643cac93c20374578b0a
nate-d-olson <nolson@nist.gov>
2022-01-26 09:53:17 -0500

    modifying handling of stratification tarball

--- scripts/run_happy.py
+++ scripts/run_happy.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # Author: Nathan Olson
+from os import path
 from snakemake.shell import shell
 
 log = snakemake.log_fmt_shell(stdout=False, stderr=True)
@@ -8,18 +9,24 @@ log = snakemake.log_fmt_shell(stdout=False, stderr=True)
 ## Optional parameters
 engine = snakemake.params.get("engine", "")
 if engine:
-    engine = "--engine {}".format(engine)
+    engine = f"--engine {engine}"
 
 truth_regions = snakemake.input.get("truth_regions", "")
 if truth_regions:
-    truth_regions = "-f {}".format(truth_regions)
+    truth_regions = f"-f {truth_regions}"
 
 target_regions = snakemake.input.get("target_regions", "")
 if target_regions:
-    target_regions = "-T {}".format(target_regions)
+    target_regions = f"-T {target_regions}"
 
 ## Extracting stratification tarball
-shell("(tar -xvf {snakemake.input.strat_tb})" "{log}")
+ref_id = snakemake.wildcards.ref_id
+strat_id = snakemake.config["stratifications"][ref_id]["id"]
+strat_dir = f"resources/stratifications/{strat_id}"
+strat_tsv = f"{strat_dir}/{snakemake.params.strat_tsv}"
+if not path.isdir(strat_dir):
+    shell("mkdir {strat_dir}", "{log}")
+    shell("tar -xvf {snakemake.input.strat_tb} -C {strat_dir})", "{log}")
 
 ## Running Happy
 shell(
@@ -28,7 +35,7 @@ shell(
     "    {engine} "
     "    -r {snakemake.input.genome}  "
     "    {truth_regions} "
-    "    --stratification {snakemake.params.strat_tsv} "
+    "    --stratification {strat_tsv} "
     "    -o {snakemake.params.prefix} "
     "    --verbose "
     "    {target_regions} "
@@ -36,6 +43,3 @@ shell(
     "    {snakemake.input.query} )"
     "{log}"
 )
-
-## Clean-up stratifications
-shell("(rm -r {snakemake.wildcards.ref_id})" "{log}")
