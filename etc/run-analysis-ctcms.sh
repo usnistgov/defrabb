eb0b1d7f2ad699b94290b7ff0365267771cab822
nate-d-olson <nolson@nist.gov>
2022-01-28 09:41:59 -0500

    fixing typos for v0.003 run

--- etc/run-analysis-ctcms.sh
+++ etc/run-analysis-ctcms.sh
@@ -2,10 +2,12 @@
 
 ## Start pipeline
 snakemake \
-	--use-conda -j 5 \
+	--use-conda -j 20 \
+	--local-cores 1 \
 	--profile etc/slurm  \
 	--verbose \
-	--printshellcmds
+	--printshellcmds \
+	--config analyses="config/0.003-analyses.tsv"
 
 ## TODO look into mounting resdata or NAS for exporting analysis output
 ## -d /working/geneteam/defrabb-runs/${time}_${repotag}  \
