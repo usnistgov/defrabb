ecb78a6f801abc4bcfe816b281e68c2c6da14b68
nolson <nolson@nist.gov>
2022-04-29 10:14:53 -0400

    WIP: Adding truvari rule unit tests
    Former-commit-id: a188f29a27c37f820656751570f81e52221d7ea1

---
+++ .tests/unit/test_run_truvari.py
@@ -0,0 +1,56 @@
+import os
+import sys
+
+import subprocess as sp
+from tempfile import TemporaryDirectory
+import shutil
+from pathlib import Path, PurePosixPath
+
+sys.path.insert(0, os.path.dirname(__file__))
+
+import common
+
+
+def test_run_truvari():
+
+    with TemporaryDirectory() as tmpdir:
+        workdir = Path(tmpdir) / "workdir"
+        data_path = PurePosixPath(".tests/unit/run_truvari/data")
+        expected_path = PurePosixPath(".tests/unit/run_truvari/expected")
+        config_path = PurePosixPath("config")
+        references_path = PurePosixPath(".tests/integration/resources/references")
+
+        # Copy data to the temporary workdir.
+        shutil.copytree(data_path, workdir)
+        shutil.copytree(config_path, workdir / "config")
+        shutil.copytree(references_path, workdir / "resources" / "references")
+
+        # dbg
+        print(
+            "results/evaluations/truvari/eval1_testA/GRCh38_chr21_v4.2.1chr21_asm17aChr21_dipcall-default/summary.txt"
+            file=sys.stderr,
+        )
+
+        # Run the test job.
+        sp.check_output(
+            [
+                "python",
+                "-m",
+                "snakemake",
+                "results/evaluations/truvari/eval1_testA/GRCh38_chr21_v4.2.1chr21_asm17aChr21_dipcall-default/summary.txt",
+                "-f",
+                "-j1",
+                "--keep-target-files",
+                "--use-conda",
+                "--directory",
+                workdir,
+            ]
+        )
+
+        # Check the output byte by byte using cmp.
+        # To modify this behavior, you can inherit from common.OutputChecker in here
+        # and overwrite the method `compare_files(generated_file, expected_file),
+        # also see common.py.
+        common.OutputChecker(
+            data_path, expected_path, workdir, ignore_unexpected=True
+        ).check()
