2e6dd002aa68d00cfbb3aa568be20ced165c22f7
nolson <nolson@nist.gov>
2022-05-10 14:15:33 -0400

    adding --touch only running tested rule
    Former-commit-id: 3dfdcd0f2f6c5b7b810f466c2ae0ae7ba4885479 [formerly 12c73fb37766b4c3cefc6a6d61c9994ccab8c289]
    
    Former-commit-id: c08822738aaaafbfa1134ba6d1b9041ea9d42c47

--- .tests/unit/test_postprocess_bed.py
+++ .tests/unit/test_postprocess_bed.py
@@ -40,6 +40,7 @@ def test_postprocess_bed():
                 "-j1",
                 "--keep-target-files",
                 "--use-conda",
+                "--touch",
                 "--directory",
                 workdir,
             ]
